9268a7b9355bc6304adc2b7f8eb068f63af89325
ulikemywa1 <calium@outlook.com>
2024-03-24 16:22:35 +0300

    feat: implement getCountDaysInMonth function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -92,8 +92,12 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  let isLeap = false;
+  if (year % 4 === 0) isLeap = true;
+  if (month === 2 && isLeap) return days[month - 1] + 1;
+  return days[month - 1];
 }
 
 /**
