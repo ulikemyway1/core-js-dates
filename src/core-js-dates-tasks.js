a3e7dbfb38e5af968d2c6d1c1a48aaa78a656a67
ulikemywa1 <calium@outlook.com>
2024-03-24 15:37:42 +0300

    feat: implement getDayName function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -45,8 +45,19 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const dateObj = new Date(date);
+  const day = dateObj.getDay();
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  return days[day];
 }
 
 /**
